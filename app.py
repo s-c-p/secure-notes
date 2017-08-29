e5fc1de48a6a101dcb4844a040e6ce24c9a53e0f
s-c-p <ps45@binkmail.com>
2017-08-29 15:03:04 +0530

    WIP now

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import pytest
+
+import utils
 import queue
 
 def crypto(file_name, direction):
-    queue.push("crypto", {
-        "file_acted_upon": file_name
-    })
+    queue.push({
+        "file_acted_upon": file_name,
+        "action": direction,
+        "old_hash": old_hash,
+        "new_hash": utils.calc_hash(file_name)
+    }, "crypto")
     return
--- setup.py
+++ setup.py
@@ -5,3 +5,9 @@
 		setup plugins
 		reactor details
 """
+
+import queue
+
+queue.define_rules("crypto",
+    [ queue.PhysicsRule(f"plugin.{crypto_plugin}", "crypto", None, "ignore")
+    , queue.PhysicsRule(f"plugin.{backup_plugin}", "backup", None, "ignore") ]) # it would be nice if crypto_plugin.crypto's return value could be added to all_params pool fir backup_plugin.backup
